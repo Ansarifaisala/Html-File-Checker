04dcb4ee6188da36c542355480f3e3d9a9d3895d
Mohammed Faisal Ansari <49450406+Ansarifaisala@users.noreply.github.com>
2022-01-03 22:56:58 -0500

    Add files via upload

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkTagsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.checktagslistBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(496, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            this.fIleToolStripMenuItem.Click += new System.EventHandler(this.FIleToolStripMenuItem_Click);
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.OpenFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkTagsToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // checkTagsToolStripMenuItem
+            // 
+            this.checkTagsToolStripMenuItem.Name = "checkTagsToolStripMenuItem";
+            this.checkTagsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkTagsToolStripMenuItem.Text = "Check Tags";
+            this.checkTagsToolStripMenuItem.Click += new System.EventHandler(this.CheckTagsToolStripMenuItem_Click);
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.stateLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.stateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stateLabel.Location = new System.Drawing.Point(13, 39);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(110, 20);
+            this.stateLabel.TabIndex = 2;
+            this.stateLabel.Text = "No File Loaded";
+            // 
+            // checktagslistBox
+            // 
+            this.checktagslistBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.checktagslistBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checktagslistBox.FormattingEnabled = true;
+            this.checktagslistBox.HorizontalScrollbar = true;
+            this.checktagslistBox.ItemHeight = 18;
+            this.checktagslistBox.Location = new System.Drawing.Point(13, 79);
+            this.checktagslistBox.Name = "checktagslistBox";
+            this.checktagslistBox.Size = new System.Drawing.Size(471, 416);
+            this.checktagslistBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 513);
+            this.Controls.Add(this.checktagslistBox);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem checkTagsToolStripMenuItem;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.ListBox checktagslistBox;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//@author Ansari Mohammed Faisal - 000812671
+namespace Lab4
+{
+    /// <summary>
+    /// this class 2 global variables
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// tagCoun is used to store  number of tags in tags
+        int tagCount = 0;
+        /// Stack tags is used to store html tags
+        Stack<string> tags = new Stack<string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FIleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// this class loads file that user slected into a string 
+        /// and then extracts all elements/tags from it into tags
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var singletonTags = new List<string> { "img", "br", "base", "area", "col", "command", "embed", "hr", "input", "keygen", "link", "meta", "param", "source", "track", "wbr" };
+            checktagslistBox.Items.Clear();
+            tags.Clear();
+            string tag = "";
+            bool start = false;
+            openFileDialog1.Filter = "HTML file (*.html)|*.html";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(openFileDialog1.FileName);
+                string filename = openFileDialog1.FileName;
+                string data = File.ReadAllText(filename);
+                stateLabel.Text = filename + " :Loaded Successfuly";
+                foreach (char c in data)
+                {
+                    if (start == true & c != '>' & c != ' ')
+                    {
+                        tag += c;
+                    }
+                    else { }
+                    if (c == '<')
+                    {
+                        start = true;
+                    }
+                    else if (c == '>' || c == ' ')
+                    {
+                        if (singletonTags.Contains(tag, StringComparer.OrdinalIgnoreCase))
+                        {
+                            tag = "";
+                            start = false;
+                        }
+                        if (tag != "")
+                        {
+                            tags.Push(tag);
+                            tag = "";
+                            start = false;
+                        }
+                    }
+                }
+                tagCount = tags.Count;
+            }
+            else
+                MessageBox.Show("You Selected No File..");
+            
+   
+
+        }
+
+
+        /// <summary>
+        /// this class prints tags on to checktagsListBox
+        /// this method also check if file is corect or not
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckTagsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stack<string> reverseTag = new Stack<string>();
+            
+                foreach (string s in tags)
+                {
+                reverseTag.Push(s);
+                }
+                foreach (string s in reverseTag)
+                {
+                    if (s.StartsWith("/"))
+                    {
+                        checktagslistBox.Items.Add("Found Closing Tag: <" + s + ">");
+                    }
+                    else
+                    {
+                        checktagslistBox.Items.Add("Found Opening Tag: <" + s + ">");
+                    }
+                }
+                if (tagCount%2 == 0)
+                {
+                    stateLabel.Text = "File is good";
+                }
+                else
+                {
+                    stateLabel.Text = "File is not good";
+                }
+
+            
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
